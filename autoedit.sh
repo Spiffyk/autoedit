6fac64480959b8e31bcb07d771841ab25cd3dfe7
Oto Šťáva <oto.stava@gmail.com>
2023-10-25 11:37:40 +0200

    Add '--reconfigure' option
    This new option runs the script non-interactively, reconfiguring the
    project. The difference between this and just running `./configure` is
    that `--reconfigure` keeps the current configuration options intact,
    while `./configure` by itself erases them.

--- autoedit.sh
+++ autoedit.sh
@@ -28,44 +28,115 @@
 
 set -e
 
-configure_command='./configure'
+short_help_text=$(cat << EOF
+USAGE: $0 [options]
+EOF
+)
 
-if [ -z "$EDITOR" ]; then
-    >&2 echo 'No $EDITOR specified.'
-    exit 1
+help_text=$(cat << EOF
+Interactive reconfiguration script for Autotools
+$short_help_text
+
+Available options:
+	-h, --help
+                Displays this help and exits.
+
+	-r, --reconfigure
+                Reconfigures the project, keeping its current configuration
+                options, without opening the editor.
+EOF
+)
+
+configure_command='./configure'
+reconfigure=0
+
+set +e
+getopt=$(getopt \
+	--options     'hr'\
+	--longoptions 'help,reconfigure'\
+	--name        'autoedit'\
+	-- "$@")
+
+if [ $? -ne 0 ]; then
+	>&2 echo "$short_help_text"
+	>&2 echo 'Use --help for more information'
+	exit 1
 fi
+set -e
+
+eval set -- "$getopt"
+unset getopt
+
+while true; do
+	case "$1" in
+		'-h'|'--help')
+			echo "$help_text"
+			exit 0
+			;;
+		'-r'|'--reconfigure')
+			reconfigure=1
+			shift
+			continue
+			;;
+		'--')
+			shift
+			break
+			;;
+		*)
+			>&2 echo 'Internal error!'
+			exit 1
+			;;
+	esac
+done
 
 if [ ! -x "$configure_command" ]; then
-    >&2 echo "'$configure_command' does not exist or it is not executable"
-    >&2 echo "HINT: did you forget to generate it with GNU Autotools?"
-    exit 1
+	>&2 echo "'$configure_command' does not exist or it is not executable"
+	>&2 echo "HINT: did you forget to generate it with GNU Autotools?"
+	exit 1
 fi
 
-tmpfile="$(mktemp)"
+if [ "$reconfigure" -eq 0 ]; then
+	if [ -z "$EDITOR" ]; then
+		>&2 echo 'No $EDITOR specified.'
+		exit 1
+	fi
 
-if [ -x "./config.status" ]; then
-    ./config.status --config >> "$tmpfile"
-    echo -e '\n' >> "$tmpfile"
-    echo $'# [info] \'config.status\' loaded - edit your configuration' >> "$tmpfile"
+	tmpfile="$(mktemp)"
+
+	if [ -x "./config.status" ]; then
+		./config.status --config >> "$tmpfile"
+		echo -e '\n' >> "$tmpfile"
+		echo $'# [info] \'config.status\' loaded - edit your configuration' >> "$tmpfile"
+	else
+		echo -e '\n' >> "$tmpfile"
+		echo $'# [info] \'config.status\' not found - provide your initial configuration' >> "$tmpfile"
+	fi
+
+	echo -e $'# [hint] put a \'@\' at the beginning of this file to cancel' >> "$tmpfile"
+	echo -e $'# [hint] everything after the first \'#\' on a line is ignored' >> "$tmpfile"
+
+	"$EDITOR" "$tmpfile"
+	args=$(grep --only-matching '^[^#]*' "$tmpfile")
+
+	# Cancel if @ is found at the beginning
+	stop_pattern='^[[:space:]]*@'
+	if [[ $args =~ $stop_pattern ]]; then
+		echo '@ found at the beginning of the temp file -- exiting'
+		rm -f "$tmpfile"
+		exit 0
+	fi
 else
-    echo -e '\n' >> "$tmpfile"
-    echo $'# [info] \'config.status\' not found - provide your initial configuration' >> "$tmpfile"
-fi
-
-echo -e $'# [hint] put a \'@\' at the beginning of this file to cancel' >> "$tmpfile"
-echo -e $'# [hint] everything after the first \'#\' on a line is ignored' >> "$tmpfile"
-
-"$EDITOR" "$tmpfile"
-args=$(grep --only-matching '^[^#]*' "$tmpfile")
-
-# Cancel if @ is found at the beginning
-stop_pattern='^[[:space:]]*@'
-if [[ $args =~ $stop_pattern ]]; then
-    echo '@ found at the beginning of the temp file -- exiting'
-    rm -f "$tmpfile"
-    exit 0
+	if [ -x "./config.status" ]; then
+		args="$(./config.status --config)"
+		echo $'\'config.status\' loaded - reconfiguring using the provided options'
+	else
+		args=''
+		echo $'\'config.status\' not found - reconfiguring with no options'
+	fi
 fi
 
 eval "$configure_command $args"
 
-rm -f "$tmpfile"
+if [ "$reconfigure" -ne 0 ]; then
+	rm -f "$tmpfile"
+fi
